951f8d67f41a31135a97f7110354e1c8d51c334e
Adisa Malik <adisamalikng0@gmail.com>
2025-02-07 13:30:51 +0100

    schedule a meeting

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import arrow from '../../assets/images/schedule_arrow.svg'
 import hero from '../../assets/images/hero_image.png'
 import "./hero.css";
+import Contactform from "../../components/contact-us/Contactform";
 
 const Hero = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
   return (
     <>
       <section className="bg-white pb-3">
@@ -15,19 +20,20 @@ const Hero = () => {
                 <p className=" leading-8 text-md md:text-xl">A visionary Documentary Filmmaker, Leadership and Strategy Expert, and passionate Climate Advocate.</p>
               </div>
               <div className="md:w-5/12 text-right">
-                <button type="button" className="px-5 py-3 text-sm capitalize font-medium text-center text-white bg-[#1E1E1E] rounded-full
+                <button type="button" onClick={openModal} className="px-5 py-3 text-sm capitalize font-medium text-center text-white bg-[#1E1E1E] rounded-full
                  hover:bg-black items-center transition-all hover:ease-in-out 
                  inline-flex hover:scale-105 duration-700 focus:ring-0 
                  focus:outline-none">schedule a meeting <img src={arrow} className="ml-2 w-3 pt-1" alt="" /></button>
               </div>
             </div>
-            
+
           </div>
         </div>
         <div>
-          <img src={hero} alt="" className="h-96 object-cover md:shadow-md w-full"/>
+          <img src={hero} alt="" className="h-96 object-cover md:shadow-md w-full" />
         </div>
-
+        {/* Modal Component */}
+        {isModalOpen && <Contactform closeModal={closeModal} />}
       </section>
     </>
   );
